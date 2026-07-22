cccbf203ece0d77e3f3455a159ec2727b3a7ce1f
강우진 <spacein1001@gmail.com>
2026-07-22 11:34:08 +0900

    Test: 논리 삭제 시각 일회성 검증 추가 [#3]

--- src/test/java/com/otboo/global/common/entity/SoftDeletableEntityTest.java
+++ src/test/java/com/otboo/global/common/entity/SoftDeletableEntityTest.java
@@ -2,7 +2,9 @@ package com.otboo.global.common.entity;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
 
+import java.time.Instant;
 import org.junit.jupiter.api.Test;
 
 class SoftDeletableEntityTest {
@@ -26,6 +28,20 @@ class SoftDeletableEntityTest {
 
         // then
         assertNotNull(entity.getDeletedAt());
+    }
+
+    @Test
+    void 논리삭제를_여러번_호출해도_최초_삭제시각을_유지한다() {
+        // given
+        TestSoftDeletableEntity entity = new TestSoftDeletableEntity();
+        entity.delete();
+        Instant firstDeletedAt = entity.getDeletedAt();
+
+        // when
+        entity.delete();
+
+        // then
+        assertSame(firstDeletedAt, entity.getDeletedAt());
     }
 
     private static class TestSoftDeletableEntity extends SoftDeletableEntity {
