3f3fc83e7a6b0d65d0fa8624e57c5e48f3b40408
tndyd123 <wjdtndyd0001@gmail.com>
2026-08-20 10:28:29 +0900

    feat: outbox도입으로 인한 listener수정

--- src/main/java/com/otboo/domain/notification/event/NotificationEventListener.java
+++ src/main/java/com/otboo/domain/notification/event/NotificationEventListener.java
@@ -1,24 +1,21 @@
 package com.otboo.domain.notification.event;
 
-import com.otboo.domain.notification.kafka.NotificationKafkaProducer;
-import com.otboo.domain.notification.service.NotificationService;
+import com.otboo.domain.notification.outbox.NotificationOutboxService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Async;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.event.TransactionPhase;
-import org.springframework.transaction.event.TransactionalEventListener;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class NotificationEventListener {
 
-  private final NotificationKafkaProducer notificationKafkaProducer;
+  private final NotificationOutboxService notificationOutboxService;
 
   // 원본 트랜잭션 커밋 이후 알림 생성/전송을 비동기로 처리
-  @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+  @EventListener
   public void handle(NotificationEvent event) {
-    notificationKafkaProducer.send(event);
+    notificationOutboxService.save(event);
   }
 }
