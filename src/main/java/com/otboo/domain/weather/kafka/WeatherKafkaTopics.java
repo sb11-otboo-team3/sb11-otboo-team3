fd260051e9ef0e43c8c0f95c6b9ce7168edc0268
Linpield <top09100620@gmail.com>
2026-08-25 11:33:10 +0900

    feat: [green] 날씨 프리패치 프로듀서 구현

---
+++ src/main/java/com/otboo/domain/weather/kafka/WeatherKafkaTopics.java
@@ -0,0 +1,9 @@
+package com.otboo.domain.weather.kafka;
+
+public final class WeatherKafkaTopics {
+
+  public static final String GRID_FORECAST_REQUESTED = "otboo.weather.prefetch-requested.v1";
+
+  private WeatherKafkaTopics() {
+  }
+}
